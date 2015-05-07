c2c15ec123f8d63a87e9a11becc27bb1108b34d6
Erick Peirson <erick.peirson@asu.edu>
2015-05-07 13:47:29 -0700

    can write and read graphs with non-builtin node objects

--- nx4j/methods.py
+++ nx4j/methods.py
@@ -1,4 +1,8 @@
 import py2neo
+import networkx as nx
+
+func_names = ['function', 'method', 'builtin_function_or_method',
+              'classmethod', 'instancemethod']
 
 
 def _get_graph(host='localhost', protocol='http', port=7474, path='db/data',
@@ -11,6 +15,36 @@ def _get_graph(host='localhost', protocol='http', port=7474, path='db/data',
     dbpath = '{0}://{1}{2}:{3}/{4}/'.format(protocol, auth, host, port, path)
     neoGraph = py2neo.Graph(dbpath)
     return neoGraph
+
+
+def _get_attrs(inst):
+    try:
+        attrs = {attr: value for attr, value in inst.__dict__.iteritems()
+                 if type(value).__name__ not in func_names
+                 and not attr.startswith('_')}
+        return attrs
+    except AttributeError:  # inst has no __dict__
+        return {}
+
+
+def _cast(node):
+    classname = node.properties['_class']
+    modname = node.properties['_module']
+
+    if modname == '':
+        _class = __builtins__[classname]
+        inst = _class(node.properties['name'])
+    else:
+        _module = getattr(__import__(modname), modname.split('.')[-1])
+        _class = getattr(_module, classname)
+        inst = _class()
+
+    for property, value in node.properties.iteritems():
+        if property.startswith('_classattr'):
+            pname = property.replace('_classattr_', '')
+            setattr(inst, pname, value)
+
+    return inst
 
 
 def write_graph(graph, *args, **kwargs):
@@ -58,6 +92,20 @@ def write_graph(graph, *args, **kwargs):
                 pass    # doesn't play nicely.
 
         neoNode = py2neo.Node(label, name=str(node))
+
+        for k, v in properties.iteritems():  # TODO: serialization?
+            neoNode.properties[k] = v
+
+        try:
+            modname = node.__module__
+        except:
+            modname = ''
+        neoNode.properties['_module'] = modname
+        neoNode.properties['_class'] = node.__class__.__name__
+
+        for attr, value in _get_attrs(node).iteritems():
+            neoNode.properties['_classattr_{0}'.format(attr)] = value
+
         neoGraph.create(neoNode)
         neoNodes[node] = neoNode
         neoNode.push()
@@ -80,5 +128,31 @@ def write_graph(graph, *args, **kwargs):
 
 
 def read_graph(*args, **kwargs):
+    query = kwargs.get('query',     # Get all nodes and edges, by default.
+                       'MATCH ()-[r]-() RETURN *')
+
+    graph = nx.MultiGraph()
+    nodes = {}
+
     neoGraph = _get_graph(*args, **kwargs)
-    return neoGraph
+    results = neoGraph.cypher.execute(query)
+    subgraph = results.to_subgraph()
+
+    for edge in subgraph.relationships:
+        if edge.start_node.properties['name'] in nodes:
+            snode = nodes[edge.start_node.properties['name']]
+        else:
+            snode = _cast(edge.start_node)
+            nodes[edge.start_node.properties['name']] = snode
+            graph.add_node(snode, edge.start_node.properties)
+
+        if edge.end_node.properties['name'] in nodes:
+            enode = nodes[edge.end_node.properties['name']]
+        else:
+            enode = _cast(edge.end_node)
+            nodes[edge.end_node.properties['name']] = enode
+            graph.add_node(enode, edge.end_node.properties)
+
+        graph.add_edge(snode, enode)
+
+    return graph
--- tests/test_nx4j.py
+++ tests/test_nx4j.py
@@ -5,19 +5,96 @@ import random
 
 from nx4j import write_graph, read_graph
 
+
+class BobNode(object):
+    def __init__(self, *args, **kwargs):
+        for k, v in kwargs.iteritems():
+            setattr(self, k, v)
+
+
+class TestReadGraph(unittest.TestCase):
+    def setUp(self):
+        neoGraph = py2neo.Graph()
+        neoGraph.delete_all()
+
+    def tearDown(self):
+        neoGraph = py2neo.Graph()
+        neoGraph.delete_all()
+
+    def test_read(self):
+        """
+        :func:`nx4j.read_graph` should return a :class:`networkx.Graph` object
+        with the appropriate number of nodes and edges.
+        """
+
+        graph = nx.generators.random_graphs.barabasi_albert_graph(20, 10)
+        i = 0
+        for node in graph.nodes():
+            graph.node[node]['a'] = i
+            graph.node[node]['size'] = 'bob'
+            graph.node[node]['label'] = 'TestNode'
+            i += 1
+
+        neoGraph = write_graph(graph)
+
+        graph2 = read_graph()
+        self.assertIsInstance(graph2, nx.Graph)
+        self.assertEqual(neoGraph.size, len(graph2.edges()))
+        self.assertEqual(neoGraph.order, len(graph2.nodes()))
+
+        for n, properties in graph2.nodes(data=True):
+            self.assertIn('a', properties)
+            self.assertIn('size', properties)
+            self.assertEqual('bob', properties['size'])
+
+    def test_read_objects(self):
+        """
+        Should be able to write NetworkX nodes that are non-native objects.
+        """
+
+        bobs = [BobNode(asdf=i) for i in xrange(21)]
+        graph = nx.Graph()
+        for i, j in zip([random.randint(0, 20) for x in xrange(40)],
+                        [random.randint(0, 20) for x in xrange(40)]):
+            graph.add_edge(bobs[i], bobs[j])
+
+        write_graph(graph)
+
+        graph2 = read_graph()
+        self.assertIsInstance(graph2, nx.Graph)
+        self.assertEqual(len(graph2.nodes()), len(graph.nodes()))
+
+        for node in graph2.nodes():
+            self.assertIsInstance(node, BobNode)
+            self.assertTrue(hasattr(node, 'asdf'))
+            self.assertIsInstance(getattr(node, 'asdf'), int)
+
+    def test_read_cypher(self):
+        """
+        Passing a kwarg `cypher` should result in a :class:`networkx.Graph`
+        object containing only nodes and edges that respond to the cypher
+        query.
+        """
+        pass
+
+
 class TestWriteGraph(unittest.TestCase):
     def setUp(self):
+        neoGraph = py2neo.Graph()
+        neoGraph.delete_all()
         self.host = 'http://localhost:7474'
+
+    def tearDown(self):
         neoGraph = py2neo.Graph()
         neoGraph.delete_all()
 
     def test_write(self):
         """
-        :func:`nx4j.write_graph` should return a :class:`py2neo.Graph` object 
+        :func:`nx4j.write_graph` should return a :class:`py2neo.Graph` object
         with the appropriate number of nodes and edges.
         """
 
-        graph = nx.generators.random_graphs.barabasi_albert_graph(20,10)
+        graph = nx.generators.random_graphs.barabasi_albert_graph(20, 10)
         neoGraph = write_graph(graph)
         self.assertIsInstance(neoGraph, py2neo.Graph)
         self.assertEqual(neoGraph.size, len(graph.edges()))
@@ -27,8 +104,26 @@ class TestWriteGraph(unittest.TestCase):
         self.assertEqual(neoGraph2.size, len(graph.edges()))
         self.assertEqual(neoGraph2.order, len(graph.nodes()))
 
-        neoGraph.delete_all()
+    def test_write_objects(self):
+        """
+        Should be able to write NetworkX nodes that are non-native objects.
+        """
 
+        bobs = [BobNode(asdf=i) for i in xrange(21)]
+        graph = nx.Graph()
+        for i, j in zip([random.randint(0, 20) for x in xrange(40)],
+                        [random.randint(0, 20) for x in xrange(40)]):
+            graph.add_edge(bobs[i], bobs[j])
+
+        neoGraph = write_graph(graph)
+        self.assertIsInstance(neoGraph, py2neo.Graph)
+        self.assertEqual(neoGraph.size, len(graph.edges()))
+        self.assertEqual(neoGraph.order, len(graph.nodes()))
+
+        subgraph = neoGraph.cypher.execute('MATCH (n) RETURN *').to_subgraph()
+
+        for node in subgraph.nodes:
+            self.assertEqual(node.properties['_class'], 'BobNode')
 
     def test_write_labels(self):
         """
@@ -36,15 +131,13 @@ class TestWriteGraph(unittest.TestCase):
         with the appropriate number of nodes and edges.
         """
 
-        graph = nx.generators.random_graphs.barabasi_albert_graph(20,10)
+        graph = nx.generators.random_graphs.barabasi_albert_graph(20, 10)
         for node in graph.nodes():
             graph.node[node]['label'] = 'TestNode'
 
         neoGraph = write_graph(graph, label_key='label')
-        self.assertEqual(len([ n for n in neoGraph.find('TestNode') ]), neoGraph.order)
-
-        neoGraph.delete_all()
-
+        self.assertEqual(len([n for n in neoGraph.find('TestNode')]),
+                         neoGraph.order)
 
     def test_write_properties(self):
         """
@@ -52,7 +145,7 @@ class TestWriteGraph(unittest.TestCase):
         with the appropriate number of nodes and edges.
         """
 
-        graph = nx.generators.random_graphs.barabasi_albert_graph(20,10)
+        graph = nx.generators.random_graphs.barabasi_albert_graph(20, 10)
         i = 0
         for node in graph.nodes():
             graph.node[node]['a'] = i
@@ -60,14 +153,11 @@ class TestWriteGraph(unittest.TestCase):
             graph.node[node]['label'] = 'TestNode'
             i += 1
 
-        neoGraph = write_graph(graph)
+        neoGraph = write_graph(graph, label_key='label')
         for n in neoGraph.find('TestNode'):
             self.assertIn('a', n.properties)
             self.assertIn('size', n.properties)
             self.assertEqual('bob', n.properties['size'])
-
-        neoGraph.delete_all()
-
 
     def test_write_digraph(self):
         """
@@ -76,17 +166,14 @@ class TestWriteGraph(unittest.TestCase):
         """
 
         graph = nx.DiGraph()
-        for i,j in zip([ random.randint(0,20) for x in xrange(40) ],
-                       [ random.randint(0,20) for x in xrange(40) ]):
-            graph.add_edge(i,j)
+        for i, j in zip([random.randint(0, 20) for x in xrange(40)],
+                        [random.randint(0, 20) for x in xrange(40)]):
+            graph.add_edge(i, j)
 
         neoGraph = write_graph(graph)
         self.assertIsInstance(neoGraph, py2neo.Graph)
         self.assertEqual(neoGraph.size, len(graph.edges()))
         self.assertEqual(neoGraph.order, len(graph.nodes()))
-
-        neoGraph.delete_all()
-
 
     def test_write_multigraph(self):
         """
@@ -95,17 +182,14 @@ class TestWriteGraph(unittest.TestCase):
         """
 
         graph = nx.MultiGraph()
-        for i,j in zip([ random.randint(0,20) for x in xrange(40) ],
-                       [ random.randint(0,20) for x in xrange(40) ]):
-            graph.add_edge(i,j)
+        for i, j in zip([random.randint(0, 20) for x in xrange(40)],
+                        [random.randint(0, 20) for x in xrange(40)]):
+            graph.add_edge(i, j)
 
         neoGraph = write_graph(graph)
         self.assertIsInstance(neoGraph, py2neo.Graph)
         self.assertEqual(neoGraph.size, len(graph.edges()))
         self.assertEqual(neoGraph.order, len(graph.nodes()))
-
-        neoGraph.delete_all()
-
 
     def test_write_multidigraph(self):
         """
@@ -114,16 +198,14 @@ class TestWriteGraph(unittest.TestCase):
         """
 
         graph = nx.MultiDiGraph()
-        for i,j in zip([ random.randint(0,20) for x in xrange(40) ],
-                       [ random.randint(0,20) for x in xrange(40) ]):
-            graph.add_edge(i,j)
+        for i, j in zip([random.randint(0, 20) for x in xrange(40)],
+                        [random.randint(0, 20) for x in xrange(40)]):
+            graph.add_edge(i, j)
 
         neoGraph = write_graph(graph)
         self.assertIsInstance(neoGraph, py2neo.Graph)
         self.assertEqual(neoGraph.size, len(graph.edges()))
         self.assertEqual(neoGraph.order, len(graph.nodes()))
-
-        neoGraph.delete_all()
 
 
 if __name__ == '__main__':
